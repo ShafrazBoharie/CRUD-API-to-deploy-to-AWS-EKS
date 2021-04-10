e7f4eedfdb89bb1fc5398b377bcd5a16365d106e
ShafrazBoharie <boharie@gmail.com>
2021-04-10 18:15:43 +0100

    .

--- src/TPICAP.TechChallenge.Model/Models/LinkDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace TPICAP.TechChallenge.Model.Models
-{
-    public record LinkDto
-    {
-        public string Href { get; init; }
-        public string Rel { get; init; }
-        public string Method { get; init; }
-    }
-}
